47c821d4a29458a3e1275afcdfe88e4109527717
shreeramkedlaya <kedlayashreeram@gmail.com>
2025-03-16 16:24:53 +0530

    Initial commit

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,14 @@
+export default function TodoInput(props) {
+    const { handleAddTodos, setTodoValue, todoValue } = props
+    return (
+        <header>
+            <input value={todoValue} onChange={(e) => {
+                setTodoValue(e.target.value)
+            }} placeholder="Enter todo..." />
+            <button onClick={() => {
+                handleAddTodos(todoValue)
+                setTodoValue('')
+            }}>Add</button>
+        </header>
+    )
+}
